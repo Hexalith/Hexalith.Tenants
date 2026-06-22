d47b2b34ed1995875b30c4c807e6f4b2e8509397
Jérôme Piquot <jpiquot@itaneo.com>
2026-06-22 13:42:12 +0200

    refactor: remove Hexalith.EventStore related classes and ProjectMetadataPaths

--- src/Hexalith.Tenants.AppHost/ProjectMetadataPaths.cs
+++
@@ -1,9 +0,0 @@
-namespace Projects;
-
-internal static class ProjectMetadataPaths {
-    public static string GetProjectPath(params string[] path)
-        => Path.Combine(GetRepositoryRoot(), Path.Combine(path));
-
-    private static string GetRepositoryRoot()
-        => Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", ".."));
-}
